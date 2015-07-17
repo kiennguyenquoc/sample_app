a5f3bb2508ab0b12c4f2467ef99c83964bd6fc6e
Nguyen Quoc Kien <quockien.mmt06@gmail.com>
2015-07-17 10:26:14 +0700

    Fix-chapter 12

--- db/schema.rb
+++ db/schema.rb
@@ -27,8 +27,8 @@ ActiveRecord::Schema.define(version: 20150716092834) do
   create_table "relationships", force: :cascade do |t|
     t.integer  "follower_id"
     t.integer  "followed_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "relationships", ["followed_id"], name: "index_relationships_on_followed_id"
@@ -38,13 +38,13 @@ ActiveRecord::Schema.define(version: 20150716092834) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",             default: false
+    t.boolean  "admin", default: false
     t.string   "activation_digest"
-    t.boolean  "activated",         default: false
+    t.boolean  "activated", default: false
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,7 @@ users = User.order(:created_at).take(6)
 end
 
 users = User.all
-user  = users.first
+user = users.first
 following = users[2..20]
 followers = users[3..20]
 following.each { |followed| user.follow(followed) }
