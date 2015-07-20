d14ccd4141b70a45a00a1752cd0551ac7b95d918
Nguyen Quoc Kien <quockien.mmt06@gmail.com>
2015-07-20 13:04:12 +0700

    fix domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "sample-app-veneer.herokuapp.com/" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -44,7 +44,7 @@ Rails.application.configure do
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.gmail.com',
-    domain: 'http://localhost:3000/',
+    domain: 'https://sample-app-veneer.herokuapp.com/',
     port: 587,
     user_name: ENV["EMAIL"],
     password: ENV["PASSWORD"],
