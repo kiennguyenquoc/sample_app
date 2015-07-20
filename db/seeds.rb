4503ca0decfdd0a2df1d10c71f66b09c7d74f853
Nguyen Quoc Kien <quockien.mmt06@gmail.com>
2015-07-20 10:51:43 +0700

    fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ User.create!(name:  "Example User",
   activated: true,
   activated_at: Time.zone.now)
 
-50.times do |n|
+99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -26,7 +26,7 @@ User.create!(name:  "Example User",
 end
 
 users = User.order(:created_at).take(6)
-40.times do
+50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
